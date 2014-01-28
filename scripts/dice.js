813d88b79e64b7ede098605bf81ddae6da3f782d
Abundnce10 <michaelstitt@gmail.com>
2014-01-27 20:35:08 -0800

    Add <p> with Goal & win/lose alert

--- scripts/dice.js
+++ scripts/dice.js
@@ -6,11 +6,18 @@
 
 var point;
 var firstturn = true;
+var nextGame = false;
+
+//Hide #goal div when starting
+$("#goal").hide();
+
 function throwdice() {
     //jquery selectors
     var $Stage = $('#stage');
     var $outcome = $('#outcome');
     var $pointValue = $('#pv');
+    var $goal = $('#goal');
+    var $firstRoll = $('#firstRoll');
 
     var sum;
     //generate random number between 2-6
@@ -26,35 +33,53 @@ function throwdice() {
     //Draw right die
     drawface(roll, '#dieRight');
 
+    if (nextGame) {
+        // started new game, empty result
+        resetGame();
+    }
+
     if (firstturn) {
         //This area holds the logic for the first turn. If outcome is not a 7,11,2,3,or 12 or store sum into point variable and return false for first turn variable
         if (sum === 7 || sum === 11) {
 
             $outcome.val('You win!!');
-
+            firstRollOutcome(true);
+            
         } else if (sum === 2 || sum === 3 || sum === 12) {
 
             $outcome.val('You Lose!!!');
+            firstRollOutcome(false);
+            
         } else {
             point = sum;
-            $pointValue.val(point);
+            $pointValue.val(point);           
             firstturn = false;
             $Stage.val('Need follow-up throw.');
             $outcome.val('  ');
+
+            // show goal with firstRoll value
+            populateGoalDiv(point);
+            nextGame = false;
+
         }
     }
-        //When user clicks again check to see if the more
+    //When user clicks again check to see if the more
     else {
         if (sum === point) {
             $Stage.val('You win!');
             $outcome.val('Back to first throw.');
             $pointValue.val(" ");
             firstturn = true;
+
+            showResult(true);
+
         } else if (sum === 7) {
             $Stage.val('Back to first throw.');
             $outcome.val('You lose!');
             $pointValue.val(" ");
             firstturn = true;
+
+            showResult(false);
 
         }
 
@@ -82,5 +107,32 @@ function throwdice() {
                 break;
         }
         $(id).css("background-position", position);
-    }
+    };
+    function populateGoalDiv(firstRoll) {
+        $("#firstRoll").html(firstRoll);
+        $("#goal").show();
+        //Show first <p> in case it's hidden (i.e. won on 1st roll)
+        $("#goal p:eq(0)").show();
+    };
+    function firstRollOutcome(success) {
+        $("#goal").show();
+        $("#goal p:eq(0)").hide();
+        showResult(success);
+    };
+    function showResult(success) {
+        var $result = $("#result");
+        if (success) {
+            $result.html("Winner");
+            $result.addClass("winner");
+        } else {
+            $result.html("Loser");
+            $result.addClass("loser");
+        }
+        //Keep track of new game, need to reset #result elem next roll
+        nextGame = true;
+    };
+    function resetGame() {
+        // remove class and empty previous result
+        $("#result").empty().removeClass('winner').removeClass('loser');
+    };
 }
